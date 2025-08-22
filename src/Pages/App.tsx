cad6c6b8cbb27af3cdb40483239ba63145bb159d
nirvana95 <masoudghari7495@gmail.com>
2025-08-22 12:39:52 +0100

    feat: :sparkles: added popularMoviesUI Component

--- src/Pages/App.tsx
+++ src/Pages/App.tsx
@@ -1,9 +1,11 @@
+import Popular from "../feature/popular/Popular";
 import Trending from "../feature/trends/Trending";
 
 function App() {
   return (
     <>
       <Trending />
+      <Popular />
     </>
   );
 }
--- src/feature/popular/Popular.tsx
+++ src/feature/popular/Popular.tsx
@@ -1,5 +1,26 @@
 function Popular() {
-  return <div></div>;
+  return (
+    <div className="all-movies">
+      <ul>
+        {Array.from({ length: 6 }).map((_, index) => (
+          <li key={index} className="movie-card">
+            <img src="/no-movie.png" alt={`Movie ${index + 1}`} />
+            <div className="content">
+              <h3>Title Movie</h3>
+            </div>
+            <div className="content">
+              <div className="rating">
+                <img src="/star.svg" />
+                <p>8.5</p>
+
+                <span>Action, Adventure, Comedy</span>
+              </div>
+            </div>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
 
 export default Popular;
